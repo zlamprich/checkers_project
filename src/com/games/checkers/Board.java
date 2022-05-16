967fda68a2c14102e7368c83bf49e1534ea95ecf
zlamprich <86210485+zlamprich@users.noreply.github.com>
2022-05-16 15:29:12 -0600

    Addition of Board static column/row values, and Array creation. Private class Pieces added to bottom of board to allow for commit/push, will revise if needed.

--- src/com/games/checkers/Board.java
+++ src/com/games/checkers/Board.java
@@ -1,5 +1,28 @@
 package com.games.checkers;
 
-class Board {
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+
+public class Board {
+    public static final int rows = 8;
+    public static final int columns = 8;
+    public int size;
+
+
+    public Pieces[][] board;
+
+    public Board () {
+        this.board = new Pieces[rows][columns];
+
+    }
+
+    public Pieces[][] getBoard() {
+        return board;
+    }
+
+    private class Pieces {
+    }
 }
