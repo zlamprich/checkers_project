d3c01e184b0d087f4a2dc252f81b07ff22ef2112
zlamprich <86210485+zlamprich@users.noreply.github.com>
2022-05-17 11:43:20 -0600

    Added methods to Board.java and Move.java.

--- src/com/games/checkers/Move.java
+++ src/com/games/checkers/Move.java
@@ -7,6 +7,7 @@ public class Move {
     // vars representing move functions
     int x1,y1;
     int x2,y2;
+    Move previousMove;
     boolean jumpMove;
 
     /*
@@ -29,5 +30,37 @@ public class Move {
         startPosition[0] = x1;
         startPosition[1] = y1;
         return startPosition;
+    }
+
+    //Method to get piece end position as coordinates in an array.
+    public int[] getEndingCoords() {
+        int[] endPosition = new int[2];
+        endPosition[0] = x2;
+        endPosition[1] = y2;
+        return endPosition;
+    }
+
+    public CheckerPiece[] getJumpedPieces(Board board) {
+        //bool to check if move was a jump move
+        if (jumpMove) {
+            //creates an arraylist of pieces.
+            ArrayList<CheckerPiece> pieces = new ArrayList<>();
+
+            //Check to see if a different piece was located in between start and end coordinates of move, resulting in a jump
+            int piecesX = (x1 + x2)/2;
+            int piecesY = (y1 + y2)/2;
+
+            //Add jump result to board without grabbing pieces from board
+            pieces.add(board.getLocationValue(piecesX, piecesY));
+
+            //Go back to get jumped pieces that were located in between x,y coords.
+            if (previousMove != null) {
+                pieces.addAll(Arrays.asList(previousMove.getJumpedPieces(board)));
+            }
+            pieces.trimToSize();
+            return pieces.toArray(new CheckerPiece[1]);
+        }
+        else
+            return null;
     }
 }
