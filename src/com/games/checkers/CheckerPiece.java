f636a63f36cd62336e4ba92821fed14446dd0008
apurslow <37603233+apurslow@users.noreply.github.com>
2022-05-17 12:30:05 -0700

    added resource folder changed CheckerPiece to use java Point class

--- src/com/games/checkers/CheckerPiece.java
+++ src/com/games/checkers/CheckerPiece.java
@@ -1,5 +1,6 @@
 package com.games.checkers;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -10,53 +11,59 @@ import java.util.Arrays;
 
 public class CheckerPiece {
     // private vars
-    private int x;
-    private int y;
+    private Point pieceCordinates;
     private boolean isKingPiece = false;
     private boolean isBlack;
 
 
-        /* Constructor to check CheckersPiece coordinates, and then
-         * check boolean true/false to see if piece is Black/P1 (true) or White/P2 (false)
-         */
-        public CheckerPiece(int x, int y, boolean b) {
-            this.x = x;
-            this.y = y;
-            this.isBlack = false;
-        }
-
-        //method to get the location of a given piece on the board, returned as an array of X, Y coordinates.
-        public int [] getPieceLocation() {
-            int[] coordinates = new int[2];
-            coordinates[0] = this.x;
-            coordinates[1] = this.y;
-            return coordinates;
-        }
-
-        //method to set piece as a king piece once reaching end of the board.
-        private void setAsKing() {
-            isKingPiece = true;
-        }
-
-        //method to CHECK if piece is a King piece after reaching the end of the board. Ran after every move.
-        public void checkKingStatus(Board board) {
-            if (isBlack && this.y == board.size - 1 || !isBlack && this.y == 0)
-                this.setAsKing();
-        }
-
-        // method to return a string of attributes related to a given piece. Checks for color, and king status.
-        public String getAttributes() {
-            String attributes;
-
-            if (isBlack)
-                attributes = "Black";
-            else
-                attributes = "White";
-            if (isKingPiece)
-                attributes += "King";
-            else
-                attributes += "Non-King";
-
-            return attributes;
-        }
+    /* Constructor to check CheckersPiece coordinates, and then
+     * check boolean true/false to see if piece is Black/P1 (true) or White/P2 (false)
+     */
+    public CheckerPiece(int x, int y, boolean b) {
+        this.pieceCordinates = new Point(x, y);
+        this.isBlack = false;
     }
+
+    //method to get the location of a given piece on the board, returned as an array of X, Y coordinates.
+    public Point getPieceLocation() {
+        return this.pieceCordinates;
+    }
+
+    //method to check if king
+    public boolean getisKingPiece() {
+        return this.isKingPiece;
+    }
+
+    public boolean getisBlack() {
+        return this.isBlack;
+    }
+
+    //method to set piece as a king piece once reaching end of the board.
+    private void setAsKing() {
+        this.isKingPiece = true;
+    }
+
+
+    //method to CHECK if piece is a King piece after reaching the end of the board. Ran after every move.
+    public void checkKingStatus(Board board) {
+        if (isBlack && pieceCordinates.getY() == board.size - 1 || !isBlack && pieceCordinates.getY() == 0)
+            this.setAsKing();
+    }
+
+
+    // method to return a string of attributes related to a given piece. Checks for color, and king status.
+    public String getAttributes() {
+        String attributes;
+
+        if (isBlack)
+            attributes = "Black";
+        else
+            attributes = "White";
+        if (isKingPiece)
+            attributes += "King";
+        else
+            attributes += "Non-King";
+
+        return attributes;
+    }
+}
