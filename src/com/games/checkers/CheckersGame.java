2a8ebf7a59b809410a645161f1602626344db558
Andy Vargas <94484667+andyvn1@users.noreply.github.com>
2022-05-18 07:36:07 -0700

    please see this one on Players

--- src/com/games/checkers/CheckersGame.java
+++ src/com/games/checkers/CheckersGame.java
@@ -1,11 +1,22 @@
 package com.games.checkers;
+import java.util.Scanner;
 
 class CheckersGame {
 
-public void playerTurn(){
+
+    private static final boolean isP1 = true;
+
+
+ public void playerTurn(){
+
+     if (isP1){
+        System.out.println("Player 1 is your turn");
+    }
+
+    else
+        System.out.println(" Player 2 is your turn");
+    }
+
 
 }
-}
-
-
 
--- src/com/games/checkers/Player.java
+++ src/com/games/checkers/Player.java
@@ -1,17 +1,27 @@
 package com.games.checkers;
 
+import java.awt.geom.Path2D;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 class Player {
 
-//Getting player names and reading them with scanner
-    public static void Players(){
+
+    public static String  playerName1;
+    public static String  playerName2;
+
+
+    //Getting player names and reading them with scanner
+    public static void Players() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Player 1: Please enter your name");
-        String P1 = scanner.nextLine();
+        String p1 = scanner.nextLine();
+
+
         System.out.println("Player 2: Please enter your name");
-        String P2 = scanner.nextLine();
-        }
+        String p2 = scanner.nextLine();
+
+
+    }
 }
